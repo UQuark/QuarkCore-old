8d098fe06fe2a12cb9b41eb267986f1041b0b4f2
Artemii Bigdan <artem@bigdan.in>
2020-04-25 22:52:34 +0300

    Split databases for mods

--- src/main/java/me/uquark/quarkcore/data/DatabaseProvider.java
+++ src/main/java/me/uquark/quarkcore/data/DatabaseProvider.java
@@ -7,9 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseProvider {
-    private static final String URL = "jdbc:h2:./quarkcore_db";
-    private static final String USER = "quarkcore";
-    private static final String PASS = "quarkcore";
+    private static final String URL = "jdbc:h2:./";
     private static Server server;
 
     public static void startServer() throws SQLException {
@@ -20,7 +18,7 @@ public class DatabaseProvider {
         server.stop();
     }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+    public static Connection getConnection(String dbName, String user, String pass) throws SQLException {
+        return DriverManager.getConnection(URL + dbName, user, pass);
     }
 }
