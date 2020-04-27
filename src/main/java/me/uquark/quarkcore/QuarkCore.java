21d2e34fcc55b595f7ad740edf66760297562da2
Artemii Bigdan <artem@bigdan.in>
2020-04-27 19:35:49 +0300

    DB in mod folder

--- src/main/java/me/uquark/quarkcore/QuarkCore.java
+++ src/main/java/me/uquark/quarkcore/QuarkCore.java
@@ -2,11 +2,18 @@ package me.uquark.quarkcore;
 
 import me.uquark.quarkcore.potion.BrewingRecipeHelper;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class QuarkCore implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger();
+    @SuppressWarnings("deprecation")
+    public static final String gameDir = FabricLoader.INSTANCE.getGameDirectory().getAbsolutePath();
+
+    public static String getModDir(String modName) {
+        return gameDir + "/mods/" + modName + "/";
+    }
 
     @Override
     public void onInitialize() {
--- src/main/java/me/uquark/quarkcore/data/DatabaseProvider.java
+++ src/main/java/me/uquark/quarkcore/data/DatabaseProvider.java
@@ -1,5 +1,6 @@
 package me.uquark.quarkcore.data;
 
+import me.uquark.quarkcore.QuarkCore;
 import org.h2.tools.Server;
 
 import java.sql.Connection;
@@ -7,7 +8,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseProvider {
-    private static final String URL = "jdbc:h2:./";
+    private static final String URL = "jdbc:h2:%s/mods/%s/%s";
     private static Server server;
 
     public static void startServer() throws SQLException {
@@ -18,7 +19,7 @@ public class DatabaseProvider {
         server.stop();
     }
 
-    public static Connection getConnection(String dbName, String user, String pass) throws SQLException {
-        return DriverManager.getConnection(URL + dbName, user, pass);
+    public static Connection getConnection(String modName, String dbName, String user, String pass) throws SQLException {
+        return DriverManager.getConnection(String.format(URL, QuarkCore.gameDir, modName, dbName), user, pass);
     }
 }
