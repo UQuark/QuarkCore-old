b034a0dc1337dc7d131ac569c4255b703ba70c3d
Artemii Bigdan <artem@bigdan.in>
2020-07-28 20:50:00 +0300

    Using Access Wideners instead of Reflection

--- src/main/java/me/uquark/quarkcore/potion/BrewingRecipeHelper.java
+++ src/main/java/me/uquark/quarkcore/potion/BrewingRecipeHelper.java
@@ -1,76 +1,22 @@
 package me.uquark.quarkcore.potion;
 
-import me.uquark.quarkcore.reflect.ReflectionHelper;
 import net.minecraft.item.Item;
 import net.minecraft.potion.Potion;
 import net.minecraft.recipe.BrewingRecipeRegistry;
 
-import java.lang.reflect.Method;
-
 public class BrewingRecipeHelper {
-    private static Method registerPotionRecipeMethod = null;
-    private static Method registerPotionTypeMethod = null;
-    private static Method registerItemRecipeMethod = null;
-
-    static {
-        initialize();
-    }
-
-    public static void initialize() {
-        registerPotionRecipeMethod = ReflectionHelper.resolveMethod(
-            BrewingRecipeRegistry.class,
-            "registerPotionRecipe",
-            "method_8074",
-            Potion.class,
-            Item.class,
-            Potion.class
-        );
-        registerPotionTypeMethod = ReflectionHelper.resolveMethod(
-            BrewingRecipeRegistry.class,
-            "registerPotionType",
-            "method_8080",
-            Item.class
-        );
-        registerItemRecipeMethod = ReflectionHelper.resolveMethod(
-            BrewingRecipeRegistry.class,
-            "registerItemRecipe",
-            "method_8071",
-            Item.class,
-            Item.class,
-            Item.class
-        );
-    }
-
     public static boolean registerPotionRecipe(Potion input, Item item, Potion output) {
-        if (registerPotionRecipeMethod == null)
-            return false;
-        try {
-            registerPotionRecipeMethod.invoke(null, input, item, output);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        BrewingRecipeRegistry.registerPotionRecipe(input, item, output);
+        return true;
     }
 
     public static boolean registerPotionType(Item item) {
-        if (registerPotionTypeMethod == null)
-            return false;
-        try {
-            registerPotionTypeMethod.invoke(null, item);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        BrewingRecipeRegistry.registerPotionType(item);
+        return true;
     }
 
     public static boolean registerItemRecipe(Item input, Item ingredient, Item output) {
-        if (registerItemRecipeMethod == null)
-            return false;
-        try {
-            registerItemRecipeMethod.invoke(null, input, ingredient, output);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        BrewingRecipeRegistry.registerItemRecipe(input, ingredient, output);
+        return true;
     }
 }
--- src/main/resources/quarkcore.aw
+++ src/main/resources/quarkcore.aw
@@ -1,1 +1,8 @@
 accessWidener	v1	named
+
+extendable method net/minecraft/recipe/BrewingRecipeRegistry registerItemRecipe (Lnet/minecraft/item/Item;Lnet/minecraft/item/Item;Lnet/minecraft/item/Item;)V
+accessible method net/minecraft/recipe/BrewingRecipeRegistry registerItemRecipe (Lnet/minecraft/item/Item;Lnet/minecraft/item/Item;Lnet/minecraft/item/Item;)V
+extendable method net/minecraft/recipe/BrewingRecipeRegistry registerPotionType (Lnet/minecraft/item/Item;)V
+accessible method net/minecraft/recipe/BrewingRecipeRegistry registerPotionType (Lnet/minecraft/item/Item;)V
+extendable method net/minecraft/recipe/BrewingRecipeRegistry registerPotionRecipe (Lnet/minecraft/potion/Potion;Lnet/minecraft/item/Item;Lnet/minecraft/potion/Potion;)V
+accessible method net/minecraft/recipe/BrewingRecipeRegistry registerPotionRecipe (Lnet/minecraft/potion/Potion;Lnet/minecraft/item/Item;Lnet/minecraft/potion/Potion;)V
