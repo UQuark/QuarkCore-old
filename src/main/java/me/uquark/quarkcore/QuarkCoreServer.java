e16ef153eeaa064302db0d6c82ebccf7bdae3abc
Artemii Bigdan <artem@bigdan.in>
2020-07-28 16:41:47 +0300

    DB Server stops on server stopping

--- src/main/java/me/uquark/quarkcore/QuarkCoreServer.java
+++ src/main/java/me/uquark/quarkcore/QuarkCoreServer.java
@@ -2,10 +2,12 @@ package me.uquark.quarkcore;
 
 import me.uquark.quarkcore.data.DatabaseProvider;
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.server.MinecraftServer;
 
 import java.sql.SQLException;
 
-public class QuarkCoreServer implements DedicatedServerModInitializer {
+public class QuarkCoreServer implements DedicatedServerModInitializer, ServerLifecycleEvents.ServerStopping {
     @Override
     public void onInitializeServer() {
         try {
@@ -14,5 +16,10 @@ public class QuarkCoreServer implements DedicatedServerModInitializer {
             QuarkCore.LOGGER.warn("Failed to launch QuarkCore DB Server");
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onServerStopping(MinecraftServer minecraftServer) {
+        DatabaseProvider.stopServer();
     }
 }
